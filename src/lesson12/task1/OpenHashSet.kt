45fcab74de476104a3c3501dbeeee1c149a47e7d
dimaboronin2004 <boronin.dd@edu.spbstu.ru>
2022-12-19 20:16:08 +0300

    new commitment

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -1,7 +1,4 @@
-@file:Suppress("UNUSED_PARAMETER")
-
 package lesson12.task1
-
 /**
  * Класс "хеш-таблица с открытой адресацией"
  *
@@ -15,38 +12,78 @@ package lesson12.task1
  *
  * В конструктор хеш-таблицы передаётся её вместимость (максимальное количество элементов)
  */
+@Suppress("UNCHECKED_CAST", "KotlinConstantConditions")
 class OpenHashSet<T>(val capacity: Int) {
+    companion object {
+        val UNINITIALIZED = Any()
+    }
+    override fun hashCode(): Int {
+        var output = 1
+        elements.forEach { pos ->
+            if (pos != UNINITIALIZED) output += pos.hashCode()
+        }
+        output = 31 * output + size
+        return output
+    }
+
+    private fun hash(element: T) = element.hashCode() % capacity
 
     /**
      * Массив для хранения элементов хеш-таблицы
      */
-    internal val elements = Array<Any?>(capacity) { null }
+    internal val elements = Array<Any?>(capacity) { UNINITIALIZED }
+
 
     /**
      * Число элементов в хеш-таблице
      */
-    val size: Int get() = TODO()
+    var size: Int = 0
+        private set
 
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean = size == 0
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        when {
+            size == capacity -> return false
+            elements[hash(element)] == UNINITIALIZED -> elements[hash(element)] = element
+            else -> {
+                (hash(element)..hash(element) + capacity).forEach { j ->
+                    if (elements[j % capacity] == element) return false
+                    when (UNINITIALIZED) {
+                        elements[j % capacity] -> {
+                            elements[j % capacity] = element
+                            size += 1
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+        size += 1
+        return true
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean = element in elements
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (!(other is OpenHashSet<*> && other.size == size)) return false
+        else for (element in elements)
+            if (element == UNINITIALIZED || contains(element as T)) return true
+        return false
+    }
 }
